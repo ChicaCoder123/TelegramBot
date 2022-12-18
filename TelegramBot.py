a7d39c8ee614b088752732f9f193925f1dcb6d97
ChicaCoder123 <91013797+ChicaCoder123@users.noreply.github.com>
2022-12-18 09:26:18 -0300

    Update TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,15 +1,14 @@
 import requests as rq
 import yaml
-import random 
+from random import choice
 
-with open('configFile.yml', 'r', encoding='utf-8') as file:
+with open('ConfigFile.yaml', 'r', encoding='utf-8') as file:
     data = yaml.safe_load(file)   
     #print(data)
 
 link = data['parameters']['apiUrl'] + data['parameters']['token']
 apiMethod = '/sendMessage'
-key = random.randint(0,4)
-msg = data['recordatorios'][key]
+msg = choice(data['recordatorios']) #Es un método que elige "al azar" un value del diccionario
 
 rq.post(link + apiMethod, data = {'chat_id': data['parameters']['chatId'], 'text' : msg})
 
