6e10a4dc106fb4ab5579bf4d88153a77bbe8c1e1
ChicaCoder123 <91013797+ChicaCoder123@users.noreply.github.com>
2022-12-18 10:11:20 -0300

    Update TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -4,16 +4,17 @@ from random import choice
 
 with open('ConfigFile.yaml', 'r', encoding='utf-8') as file:
     data = yaml.safe_load(file)   
+    baseUrl = data['parameters']['apiUrl']
     #print(data)
 
-link = data['parameters']['apiUrl'] + data['parameters']['token']
+link = baseUrl + data['parameters']['token'] 
+#Se debería hacer con urllib.parse.join() en realidad y no con '+' pero 
+#no concatenaba los urls como yo quería
 apiMethod = '/sendMessage'
 msg = choice(data['recordatorios']) #Es un método que elige "al azar" un value del diccionario
 
+
 rq.post(link + apiMethod, data = {'chat_id': data['parameters']['chatId'], 'text' : msg})
-
-
-
 
 
 
